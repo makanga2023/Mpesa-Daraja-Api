e244665faff866f30c9edbd6950a601aa74257fd
Antony <antonymakanga88@gmail.com>
2023-06-19 22:26:44 +0300

    sending the accesstoken file

---
+++ accessToken.php
@@ -0,0 +1,19 @@
+<?php
+//YOU MPESA API KEYS
+$consumerKey = "M2Lq9dVY6Y1JIQa7rAVXhKOajknnuIi9"; //Fill with your app Consumer Key
+$consumerSecret = "cXLPecSeG3rMxzbl"; //Fill with your app Consumer Secret
+//ACCESS TOKEN URL
+$access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+$headers = ['Content-Type:application/json; charset=utf8'];
+$curl = curl_init($access_token_url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($curl, CURLOPT_HEADER, FALSE);
+curl_setopt($curl, CURLOPT_USERPWD, $consumerKey . ':' . $consumerSecret);
+$result = curl_exec($curl);
+$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+//echo $result;
+$result = json_decode($result);
+// ASSIGN ACCESS TOKEN TO A VARIABLE
+$access_token = $result->access_token;
+curl_close($curl);
